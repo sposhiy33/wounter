2dfbbb1ea0cf80c96ceec2265ce4bbbd0e7e8d7d
Shrey Poshiya <shreyposh@gmail.com>
2025-09-27 13:46:59 -0600

    more functionality for compatibility with render web service hosting

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def upload_image():
 
 # This allows running the app directly from the script
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
